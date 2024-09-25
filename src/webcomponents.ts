89b4012ec7460becc8430aa4815a18146740b931
Armando Albor <armandoalbor@gmail.com>
2024-09-24 20:50:19 -0500

    initial commit

---
+++ src/webcomponents.ts
@@ -0,0 +1,12 @@
+// import React from "react";
+// import * as ReactDOMClient from "react-dom/client";
+// // import reactToWebComponent from "react-to-webcomponent";
+
+// import { Header } from "./Header";
+
+// customElements.define(
+//   "rwc-header",
+// //   reactToWebComponent(Header, React, ReactDOMClient, {
+// //     props: ["text"],
+// //   })
+// );
